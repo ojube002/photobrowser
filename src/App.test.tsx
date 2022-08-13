5a6a7215e39fe8baa02b2abcfc78cb9f3c1aff37
Julien Bergström <julien.93@hotmail.fi>
2022-08-13 16:29:40 +0300

    Fixed the only test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders title', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const titleElement = screen.getByText(/Photobrowser/i);
+  expect(titleElement).toBeInTheDocument();
 });
